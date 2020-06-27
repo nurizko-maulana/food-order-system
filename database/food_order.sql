8688f4fdda28dcf82aa04133009fc88cc1259766
Nurizko Maulana <nurizko.maulana@gmail.com>
2020-06-27 13:13:23 +0700

    menambahkan database

--- database/food_order.sql
+++ database/food_order.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 20, 2020 at 01:01 AM
+-- Generation Time: Jun 27, 2020 at 08:09 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
@@ -20,6 +20,48 @@ SET time_zone = "+00:00";
 --
 -- Database: `food_order`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `cust_id` int(11) NOT NULL,
+  `cust_name` varchar(100) NOT NULL,
+  `cust_pass` varchar(100) NOT NULL,
+  `cust_address` varchar(255) NOT NULL,
+  `postcode` int(10) NOT NULL,
+  `town` varchar(100) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `cust_status` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `emp_id` int(11) NOT NULL,
+  `emp_name` varchar(100) NOT NULL,
+  `emp_pass` varchar(100) NOT NULL,
+  `emp_status` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `feedbackNo` int(11) NOT NULL,
+  `cust_id` int(11) NOT NULL,
+  `fdk_message` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -42,24 +84,71 @@ INSERT INTO `menu` (`menu_id`, `menu`, `price`) VALUES
 (2, 'Nasi Ayam', '5.00'),
 (3, 'Bubur Ayam', '5.00'),
 (4, 'Nasi Goreng', '4.00'),
-(5, 'Nasi Arab', '8.00');
+(5, 'Nasi Arab', '8.00'),
+(6, 'Nasi Kebuli', '5.00');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `orderdetail`
 --
 
-CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `role` varchar(50) NOT NULL
+CREATE TABLE `orderdetail` (
+  `order_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `order_qty` int(11) NOT NULL,
+  `total_price` decimal(13,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `order_date` date NOT NULL DEFAULT current_timestamp(),
+  `order_status` varchar(10) NOT NULL,
+  `cust_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `payment_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `cust_id` int(11) NOT NULL,
+  `payment_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `payment_method` varchar(10) NOT NULL,
+  `total_amount` decimal(13,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`cust_id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`emp_id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`feedbackNo`);
 
 --
 -- Indexes for table `menu`
@@ -68,26 +157,89 @@ ALTER TABLE `menu`
   ADD PRIMARY KEY (`menu_id`);
 
 --
--- Indexes for table `user`
+-- Indexes for table `orderdetail`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`);
+ALTER TABLE `orderdetail`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `menu_id_fk` (`menu_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `customer_id_fk` (`cust_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`payment_id`),
+  ADD KEY `order_id_fk` (`order_id`),
+  ADD KEY `cust_id` (`cust_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `cust_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `feedbackNo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `menu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `menu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT for table `orders`
 --
-ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orderdetail`
+--
+ALTER TABLE `orderdetail`
+  ADD CONSTRAINT `menu_id_fk` FOREIGN KEY (`menu_id`) REFERENCES `menu` (`menu_id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `customer_id_fk` FOREIGN KEY (`cust_id`) REFERENCES `customer` (`cust_id`);
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `order_id_fk` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`cust_id`) REFERENCES `customer` (`cust_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- src/food/order/system/FoodOrderSystem.java
+++ src/food/order/system/FoodOrderSystem.java
@@ -3,13 +3,16 @@ package food.order.system;
  *
  * @author Nurizko Maulana
  */
+import java.util.Date;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+
 
 public class FoodOrderSystem {
 
@@ -43,19 +46,23 @@ public class FoodOrderSystem {
             stmt.close();
             conn.close();
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.print(e);
         }
 
     }
 
     static void showMenu() {
         System.out.println("\n========= MENU UTAMA =========");
-        System.out.println("1. Insert Data");
-        System.out.println("2. Show Data");
-        System.out.println("3. Edit Data");
-        System.out.println("4. Delete Data");
-        System.out.println("0. Keluar");
+        System.out.println("1. Insert menu");
+        System.out.println("2. Show menu");
+        System.out.println("3. Edit menu");
+        System.out.println("4. Delete menu");
+        System.out.println("5. Insert Order");
+        System.out.println("6. Show Order");
+        System.out.println("7. Edit Order");
+        System.out.println("8. Delete Order");
+        System.out.println("0. Exit");
         System.out.println("");
         System.out.print("PILIHAN> ");
 
@@ -67,27 +74,39 @@ public class FoodOrderSystem {
                     System.exit(0);
                     break;
                 case 1:
-                    insertBuku();
+                    insertMenu();
                     break;
                 case 2:
-                    showData();
+                    listMenu();
                     break;
                 case 3:
-                    updateBuku();
+                    updateMenu();
                     break;
                 case 4:
-                    deleteBuku();
+                    deleteMenu();
+                    break;
+                case 5:
+                    insertOrder();
+                    break;
+                case 6:
+                    listOrder();
+                    break;
+                case 7:
+                    updateOrder();
+                    break;
+                case 8:
+                    deleteOrder();
                     break;
                 default:
                     System.out.println("Pilihan salah!");
 
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException | NumberFormatException e) {
+            System.out.print(e);
         }
     }
 
-    static void showData() {
+    static void listMenu() {
         String sql = "SELECT * FROM menu";
 
         try {
@@ -105,13 +124,12 @@ public class FoodOrderSystem {
                 System.out.println(String.format("%d. %s -- (%s)", idmenu, nama_menu, harga_menu));
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println(e);
         }
-
     }
 
-    static void insertBuku() {
+    static void insertMenu() {
         try {
             // ambil input dari user
             System.out.print("Nama Menu: ");
@@ -126,13 +144,13 @@ public class FoodOrderSystem {
             // simpan buku
             stmt.execute(sql);
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException | NumberFormatException | SQLException e) {
+            System.out.println(e);
         }
 
     }
 
-    static void updateBuku() {
+    static void updateMenu() {
         try {
 
             // ambil input dari user
@@ -150,12 +168,12 @@ public class FoodOrderSystem {
             // update data buku
             stmt.execute(sql);
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException | NumberFormatException | SQLException e) {
+            System.out.println(e);
         }
     }
 
-    static void deleteBuku() {
+    static void deleteMenu() {
         try {
 
             // ambil input dari user
@@ -169,8 +187,89 @@ public class FoodOrderSystem {
             stmt.execute(sql);
 
             System.out.println("Data telah terhapus...");
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException | NumberFormatException | SQLException e) {
+            System.out.println(e);
+        }
+    }
+    static void listOrder() {
+        String sql = "SELECT * FROM menu";
+
+        try {
+            rs = stmt.executeQuery(sql);
+
+            System.out.println("+--------------------------------+");
+            System.out.println("|    DATA MENU DI KEDAI MAK CIK   |");
+            System.out.println("+--------------------------------+");
+
+            while (rs.next()) {
+                int idmenu = rs.getInt("menu_id");
+                String nama_menu = rs.getString("menu");
+                String harga_menu = rs.getString("price");
+
+                System.out.println(String.format("%d. %s -- (%s)", idmenu, nama_menu, harga_menu));
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+    static void insertOrder() {
+        try {
+            // ambil input dari user
+            System.out.print("ID Menu: ");
+            String menu = input.readLine().trim();
+            System.out.print("Quantity: ");
+            String harga = input.readLine().trim();
+
+            // query simpan
+            String sql = "INSERT INTO menu (menu, price) VALUE('%s', '%s')";
+            sql = String.format(sql, menu, harga);
+
+            // simpan buku
+            stmt.execute(sql);
+
+        } catch (IOException | NumberFormatException | SQLException e) {
+            System.out.println(e);
+        }
+    }
+    static void updateOrder() {
+        try {
+
+            // ambil input dari user
+            System.out.print("ID yang mau diedit: ");
+            int idMenu = Integer.parseInt(input.readLine());
+            System.out.print("Nama menu: ");
+            String menu = input.readLine().trim();
+            System.out.print("harga: ");
+            String harga = input.readLine().trim();
+
+            // query update
+            String sql = "UPDATE menu SET name='%s', price='%s' WHERE menu_id=%d";
+            sql = String.format(sql, menu, harga, idMenu);
+
+            // update data buku
+            stmt.execute(sql);
+
+        } catch (IOException | NumberFormatException | SQLException e) {
+            System.out.println(e);
+        }
+    }
+    static void deleteOrder() {
+        try {
+
+            // ambil input dari user
+            System.out.print("ID yang mau dihapus: ");
+            int idMenu = Integer.parseInt(input.readLine());
+
+            // buat query hapus
+            String sql = String.format("DELETE FROM menu WHERE menu_id=%d", idMenu);
+
+            // hapus data
+            stmt.execute(sql);
+
+            System.out.println("Data telah terhapus...");
+        } catch (IOException | NumberFormatException | SQLException e) {
+            System.out.println(e);
         }
     }
 }
